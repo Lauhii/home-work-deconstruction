6509acab29d6f1f43fd97043a3529ff4b15ae08c
Yossef <chipeurrr@gmail.com>
2024-02-12 19:23:30 +0200

    finished homework

---
+++ script.js
@@ -0,0 +1,48 @@
+// 1st mission 
+// let PersonA = {firstName : 'Yossef',lastName: 'Assouline'};
+// function getFullName(object){
+//     const { firstName : name , lastName : last} = PersonA;
+//     console.log(`${name} ${last}`)
+// }
+// getFullName(PersonA);
+
+// 2nd mission
+
+// let person = {name:'Yossef',age:19};
+// function canVote(person){
+//     const {age} = person;
+//     if(`${age}` >= 18){
+//        console.log("true");
+//     }
+//     else {console.log("false")};
+    
+// }
+// canVote(person)
+
+// 3rd mission
+// let numbers = [1,2,3,4,5,6,7,8]
+// let firstLast = [];
+// function getFirstTwo(arrofnum){
+//     const [first , second] = numbers;
+//     firstLast.push(`${first}`, `${second}`);
+//     console.log(firstLast)
+// }
+// getFirstTwo(numbers)
+
+// 4th mission
+// let rectangle = {width:800 ,height:400}
+// function getArea(rectangle){
+//     const {width,height} = rectangle;
+//     console.log(`${width}`*`${height}`)
+// }
+// getArea(rectangle);
+// 5th mission
+// let rectangle = {x:250 ,y:300}
+// let extractedValue = [];
+// function getCoordinates(Coordinates){
+//     const {x = 0,y = 0} = rectangle;
+//     extractedValue.push(`${x}`, `${y}`);
+//     console.log(extractedValue)
+
+// }
+// getCoordinates(rectangle);
